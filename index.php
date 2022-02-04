37f3daee5f7e5dcb62fa7e3ba88591b95cf2791c
mark5041 <mark.beloso5041@gmail.com>
2022-02-04 12:49:19 +0100

    db

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+    $auto = [
+        [
+            'codice_auto' => 0,
+            'Targa' => 'EE000HG',
+            'Tipo' => 'piccola',
+            'Modello' => 'fiat panda',
+            'Persone' => 4,
+            'Porte' => 5,
+            'Bagagli' => 1,
+            'Cambio' => 'manuale',
+            'Condizionatore' => 0,
+            'Kilometri' => 125.50,
+            'foto' => 'https://www.autoscout24.it/assets/auto/images/model/fiat/fiat-panda/fiat-panda-l-01.jpg'
+        ],
+        [
+            'codice_auto' => 1,
+            'Targa' => 'AB256TH',
+            'Tipo' => 'SUV',
+            'Modello' => 'opel kuga',
+            'Persone' => 5,
+            'Porte' => 5,
+            'Bagagli' => 3,
+            'Cambio' => 'manuale',
+            'Condizionatore' => 0,
+            'Kilometri' => 155.50,
+            'foto' => 'https://www.autoscout24.it/assets/auto/images/model/ford/ford-kuga/ford-kuga-l-04.jpg'
+        ],
+        [
+            'codice_auto' => 2,
+            'Targa' => 'RB206TB',
+            'Tipo' => 'media',
+            'Modello' => 'toyota auris',
+            'Persone' => 5,
+            'Porte' => 5,
+            'Bagagli' => 2,
+            'Cambio' => 'automatico',
+            'Condizionatore' => 0,
+            'Kilometri' => 130.50,
+            'foto' => 'https://www.autoscout24.it/assets/auto/images/model/vw/vw-tiguan/vw-tiguan-l-01.jpg'
+        ],
+    ]
+
+?>
